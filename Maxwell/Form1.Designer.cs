7fb07494aa4a195e171c6f63978f0d8427c766d2
ogrimt <71195903+ogrimt@users.noreply.github.com>
2021-04-20 08:46:00 +0300

    Add files via upload

---
+++ Maxwell/Form1.Designer.cs
@@ -0,0 +1,413 @@
+﻿namespace Maxwell
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.exit_button = new System.Windows.Forms.Button();
+            this.stop_button = new System.Windows.Forms.Button();
+            this.start_button = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.результатToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.посмотретьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.textBox8);
+            this.panel1.Controls.Add(this.exit_button);
+            this.panel1.Controls.Add(this.stop_button);
+            this.panel1.Controls.Add(this.start_button);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 450);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(801, 47);
+            this.panel1.TabIndex = 0;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(532, 17);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(123, 13);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Прошедшее время, нс:";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(661, 12);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 3;
+            // 
+            // exit_button
+            // 
+            this.exit_button.Location = new System.Drawing.Point(234, 12);
+            this.exit_button.Name = "exit_button";
+            this.exit_button.Size = new System.Drawing.Size(75, 23);
+            this.exit_button.TabIndex = 2;
+            this.exit_button.Text = "Выход";
+            this.exit_button.UseVisualStyleBackColor = true;
+            this.exit_button.Click += new System.EventHandler(this.exit_button_Click);
+            // 
+            // stop_button
+            // 
+            this.stop_button.Enabled = false;
+            this.stop_button.Location = new System.Drawing.Point(143, 12);
+            this.stop_button.Name = "stop_button";
+            this.stop_button.Size = new System.Drawing.Size(75, 23);
+            this.stop_button.TabIndex = 1;
+            this.stop_button.Text = "Стоп";
+            this.stop_button.UseVisualStyleBackColor = true;
+            this.stop_button.Click += new System.EventHandler(this.stop_button_Click);
+            // 
+            // start_button
+            // 
+            this.start_button.Location = new System.Drawing.Point(44, 12);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(75, 23);
+            this.start_button.TabIndex = 0;
+            this.start_button.Text = "Пуск";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.textBox7);
+            this.panel2.Controls.Add(this.textBox6);
+            this.panel2.Controls.Add(this.textBox5);
+            this.panel2.Controls.Add(this.textBox4);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.menuStrip1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(188, 450);
+            this.panel2.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(41, 401);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Масштаб, пикс/нм:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(43, 335);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Временной шаг, нс:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(41, 273);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Ширина области, нм:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Длина области, нм:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(168, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Температурный параметр, м/с:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Радиус частиц, нм";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Количество частиц:";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(41, 417);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(104, 20);
+            this.textBox7.TabIndex = 6;
+            this.textBox7.Text = "40";
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(42, 351);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(104, 20);
+            this.textBox6.TabIndex = 5;
+            this.textBox6.Text = "0.0001";
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(41, 289);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(104, 20);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.Text = "10";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(42, 226);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(104, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "10";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(44, 161);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(104, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "200";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(45, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(104, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "0.2";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(104, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "100";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.открытьToolStripMenuItem,
+            this.результатToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(188, 24);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem1,
+            this.сохранитьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem1
+            // 
+            this.открытьToolStripMenuItem1.Name = "открытьToolStripMenuItem1";
+            this.открытьToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.открытьToolStripMenuItem1.Text = "Открыть";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(12, 20);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(188, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(613, 450);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // результатToolStripMenuItem
+            // 
+            this.результатToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.посмотретьToolStripMenuItem});
+            this.результатToolStripMenuItem.Name = "результатToolStripMenuItem";
+            this.результатToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.результатToolStripMenuItem.Text = "Результат";
+            // 
+            // посмотретьToolStripMenuItem
+            // 
+            this.посмотретьToolStripMenuItem.Enabled = false;
+            this.посмотретьToolStripMenuItem.Name = "посмотретьToolStripMenuItem";
+            this.посмотретьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.посмотретьToolStripMenuItem.Text = "Посмотреть";
+            this.посмотретьToolStripMenuItem.Click += new System.EventHandler(this.посмотретьToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 497);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Распределение Максвелла";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button exit_button;
+        private System.Windows.Forms.Button stop_button;
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem результатToolStripMenuItem;
+        public System.Windows.Forms.ToolStripMenuItem посмотретьToolStripMenuItem;
+    }
+}
+
---
+++ Maxwell/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maxwell
+{
+    public partial class Form1 : Form
+    {
+
+        public static Form1 ff1;
+        public static Form2 ff2;
+
+        int N;
+        double R, Vmax, Xmax, Ymax, dt, K;
+        double[] X, Y, Vx, Vy;
+        Random rndm = new Random();
+
+        double p_x, p_y, p2, V_par1_x, V_par1_y, V_perp1_x, V_perp1_y, V_par2_x, V_par2_y, V_perp2_x, V_perp2_y;
+
+        private void посмотретьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ff2 = new Form2();
+            ff2.Visible = true;
+            посмотретьToolStripMenuItem.Enabled = false;
+        }
+
+        int t; 
+        double[] CS;
+        int[] CN;
+
+        Bitmap bmp1, bmp2;
+        Graphics gr1, gr2;
+        Pen P1 = new Pen(Color.Red, 2);
+        Pen P2 = new Pen(Color.Green, 2);
+        SolidBrush Br = new SolidBrush(Color.Yellow);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ff1 = this;
+            bmp1 = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            gr1 = Graphics.FromImage(bmp1);
+            bmp2 = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            gr2 = Graphics.FromImage(bmp2);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            t = 0;
+            N = int.Parse(textBox1.Text);
+            R = double.Parse(textBox2.Text);
+            Vmax = double.Parse(textBox3.Text);
+            Xmax = double.Parse(textBox4.Text) / 2;
+            Ymax = double.Parse(textBox5.Text) / 2;
+            dt = double.Parse(textBox6.Text);
+            K = double.Parse(textBox7.Text);
+
+            X = new double[N];
+            Y = new double[N];
+            Vx = new double[N];
+            Vy = new double[N];
+            CS = new double[N];
+            CN = new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                CS[i] = 0;
+                CN[i] = 0;
+
+                X[i] = (2*rndm.NextDouble() - 1) * (Xmax - R);
+                Y[i] = (2*rndm.NextDouble() - 1) * (Ymax - R);
+                Vx[i] = (2 * rndm.NextDouble() - 1) * Vmax;
+                Vy[i] = (2 * rndm.NextDouble() - 1) * Vmax;
+            }
+
+
+            gr1.Clear(pictureBox1.BackColor);
+            gr2.Clear(pictureBox1.BackColor);
+            // Xscr = (int) (bmp1.width /2 + K * X)
+            // Yscr = (int) (bmp1.width /2 - K * Y)
+
+            //Двумерный случай
+            gr1.DrawRectangle(P2, (int)(bmp1.Width/2 - K*Xmax), (int)(bmp1.Height / 2 - K * Ymax), (int)(2*K*Xmax), (int)(2*K*Ymax));
+            gr2.DrawImage(bmp1, 0, 0);
+
+            for(int i = 0; i < N; i++)
+            {
+                gr2.DrawEllipse(P1, (int)(bmp1.Width / 2 + K * (X[i] - R)), (int)(bmp1.Height / 2 - K * (Y[i] + R)), (int)(2 * K * R), (int)(2 * K * R));
+                gr2.FillEllipse(Br, (int)(bmp1.Width / 2 + K * (X[i] - R)), (int)(bmp1.Height / 2 - K * (Y[i] + R)), (int)(2 * K * R), (int)(2 * K * R));
+            }
+            pictureBox1.Image = bmp2;
+
+            start_button.Enabled = false;
+            stop_button.Enabled = true;
+            посмотретьToolStripMenuItem.Enabled = true;
+            timer.Enabled = true;
+        }
+
+        private void stop_button_Click(object sender, EventArgs e)
+        {
+            start_button.Enabled = true;
+            stop_button.Enabled = false;
+            timer.Enabled = false;
+        }
+
+        private void exit_button_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            t++;
+            textBox8.Text = (t * dt).ToString();
+            //Движение частиц
+            for (int i = 0; i < N; i++)
+            {
+                X[i] += Vx[i] * dt;
+                Y[i] += Vy[i] * dt;
+                CS[i] += Math.Sqrt(Vx[i] * Vx[i] + Vy[i] * Vy[i])*dt;
+                // Проверяем соударение со стенками
+                if ((X[i] >= Xmax - R) && (Vx[i] > 0)) Vx[i] = -Vx[i];
+                if ((X[i] <= -Xmax + R) && (Vx[i] < 0)) Vx[i] = -Vx[i];
+                if ((Y[i] >= Ymax - R) && (Vy[i] > 0)) Vy[i] = -Vy[i];
+                if ((Y[i] <= -Ymax + R) && (Vy[i] < 0)) Vy[i] = -Vy[i];
+            }
+
+
+            //Соударение частиц между собой
+            //i - 1, j - 2
+            for (int i = 0; i<N-1; i++)
+            {
+                for (int j = i + 1; j < N; j++)
+                {
+                    p_x = X[j] - X[i];
+                    p_y = Y[j] - Y[i];
+                    p2 = p_x * p_x + p_y * p_y;
+                    if (p2 <= 4 * R * R)
+                    {
+                        V_par1_x = (Vx[i] * p_x + Vy[i] * p_y) * p_x / p2;
+                        V_par1_y = (Vx[i] * p_x + Vy[i] * p_y) * p_y / p2;
+                        V_par2_x = (Vx[j] * p_x + Vy[j] * p_y) * p_x / p2;
+                        V_par2_y = (Vx[j] * p_x + Vy[j] * p_y) * p_y / p2;
+
+                        if ((V_par2_x - V_par1_x)*p_x + (V_par2_y - V_par1_y) * p_y < 0) 
+                        {
+                            V_perp1_x = Vx[i] - V_par1_x;
+                            V_perp1_y = Vy[i] - V_par1_y;
+                            V_perp2_x = Vx[j] - V_par2_x;
+                            V_perp2_y = Vy[j] - V_par2_y;
+
+
+                            Vx[i] = V_par2_x + V_perp1_x;
+                            Vy[i] = V_par2_y + V_perp1_y;
+                            Vx[j] = V_par1_x + V_perp2_x;
+                            Vy[j] = V_par1_y + V_perp2_y;
+                            CN[i]++;
+                            CN[j]++;
+                        }
+                    }
+                }
+            }
+
+
+            gr2.DrawImage(bmp1, 0, 0);
+            for (int i = 0; i < N; i++)
+            {
+                gr2.DrawEllipse(P1, (int)(bmp1.Width / 2 + K * (X[i] - R)), (int)(bmp1.Height / 2 - K * (Y[i] + R)), (int)(2 * K * R), (int)(2 * K * R));
+                gr2.FillEllipse(Br, (int)(bmp1.Width / 2 + K * (X[i] - R)), (int)(bmp1.Height / 2 - K * (Y[i] + R)), (int)(2 * K * R), (int)(2 * K * R));
+            }
+            pictureBox1.Image = bmp2;
+        }
+    }
+}
